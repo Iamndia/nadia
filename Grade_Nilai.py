d0e4bb2d417b83bfbfbb15a5a15d29028199a161
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-07 20:01:04 +0800

    Menghitung grade
    ...

---
+++ Grade_Nilai.py
@@ -0,0 +1,12 @@
+#cara melakukan perhitungan aritmetikap
+
+
+nilai = int(input("masukkan nilai ujian = "))
+if ( nilai >= 85 and nilai <=100 ):
+   print("nilai A")
+elif ( nilai >= 80 and nilai <=89 ):
+    print("Nilai B")
+elif ( nilai >= 70 and nilai <= 79 ):
+    print("Nilai C")
+elif ( nilai < 60):
+    print("Nilai D")
