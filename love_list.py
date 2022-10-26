894278b487cb1f6e6181c6797d30623e29d73aad
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-26 18:57:15 +0800

    Add files via upload

---
+++ love_list.py
@@ -0,0 +1,2 @@
+letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+print('heyy,,',letter[8],' ',letter[11], letter [14],letter[21],letter[4],' ',letter[24],letter[14],letter[20])
